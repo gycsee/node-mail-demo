97ad7dd56056c55c1aa83255db29ec5ca5eff63d
CaiYiLiang <877526529@qq.com>
2017-02-26 20:53:14 +0800

    avoid mail-block issue

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
 const mail = require('nodemailer');
 const log4js = require('log4js');
-const mailOptions = require('./src/mailOptions');
+const mailInfo = require('./src/mailContext');
+const parseExcel = require('./src/parseExcel');
 
 //config log4js
 log4js.configure('log4js_confg.json');
 var logger = log4js.getLogger('nodemailer');
 
 const transport = mail.createTransport({
-      host:'smtp.gmail.com',      //mail service mail host
-      domains: ["gmail.com", "googlemail.com"],
+      host: mailInfo.host,      //mail service mail host
+      domains: mailInfo.domains,
       secureConnection:true,      //secureConnection 使用安全连接
-      port:465,                   //port STMP端口号
+      port: mailInfo.port,                   //port STMP端口号
       auth:{
-        user:"userAccount@gmail.com", //gmail address 
-        pass:"XXXXXX"     //gmail pw
+        user: mailInfo.userAcount, //Email address 
+        pass: mailInfo.userPassword //Email pw
      },
      debug: true
 });
 
-transport.sendMail(mailOptions.options,function (err, res) {
-    if(err) {console.log(err); logger.debug(err.Error);}
-    else {
-          console.log(`发送成功！`);
-          console.log(res);
-          logger.info(`Mail has sent to : ${res.accepted}`);
-        }
-    //transport.close(); // 如果没用，关闭连接池
-  });
+function sendMail(mailAddress){
+  return function(){
+  let options = {
+      from:mailInfo.mailFrom,
+      to: mailAddress, 
+      subject: mailInfo.subject,    
+      html: mailInfo.html_context,
+      attachments: 
+                  [
+                      {
+                          filename: 'QR.jpg',            // attachment_name
+                          path: 'img/QR.jpg',            // attachment_path
+                          cid : '00000001'               // cid index by mail
+                      }
+                  ]
+      };
+
+    transport.sendMail(options,function (err, res) {
+      if(err) {
+               console.log(err); 
+               logger.debug(`${err.errno} - ${mailAddress}`); 
+              }
+      else {
+              console.log(res);
+              console.log(`Sucess！--------------------------
+                          `);
+              logger.info(`Success - ${res.accepted}`);
+          }
+    });
+    
+  }
+}
+
+//To avoid mail service party block your mails when you sent to their port at a time
+parseExcelmailList.forEach(mailAddress => {
+    setTimeout(sendMail(mailAddress), 5000);
+});
+
+
+
+
+
--- src/mailContext.js
+++ src/mailContext.js
@@ -1,11 +1,26 @@
+const host = "smtp.gmail.com";
+const domains = ["gmail.com", "googlemail.com"];
+const port = 465;
+
+const userAcount = "xxxx@gmail.com";
+const userPassword = "xxxxxx";
+
+const mailFrom = "fcc樱桃妹 freecodecampguangzhou@gmail.com" ;
+const subject = "fcc-广州线下面基(25号-周六)提醒";
+
 var html_context = [];
 
-html_context[0] = "<h3>This is the demo for Node-xlxs-mailer！</h3>";
-// html_context[1] = "<div><b>...</b> ... </div>"
-// html_context[2] = "<div><b>...</b> ... </div>"
-// html_context[3] = "<div><b>...</b> ... </div>"
-// ......
+html_context[0] = "<h3>Mail Sent！！！</h3>";
+html_context[1] = "<div><i>本次邮件由nodexlxs & nodemailer开发的脚本自动发送,懒惰万岁！！</i></div>";
+html_context[2] = "<div><i>Github Repo: <a href='https://github.com/CaiYiLiang/Node-xlxs-mail' target='_blank' style='color:rgb(51, 102, 255)'>CaiYiLiang/Node-xlxs-mail</a></i></div>";
+html_context[3] = "<div><i>如果你喜欢请给一个小星星star，我应该会高兴1小时-60分钟-3600秒O(∩_∩)O</i></div>";
+//......
 
-exports.getContext = function(){
-    return html_context.join("");
-   };
+exports.host = host;
+exports.domains = domains;
+exports.port = port;
+exports.userAcount = userAcount;
+exports.userPassword = userPassword;
+exports.mailFrom = mailFrom;
+exports.subject = subject;
+exports.html_context = html_context.join("");
